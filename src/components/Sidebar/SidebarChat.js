802bb1ad6cd0cbe076b30490dfde677ca1ddeaee
Ahmadjon <info.ahmadjon@gmail.com>
2020-11-16 11:54:01 +0500

    sidebar header styled

--- src/components/Sidebar/SidebarChat.js
+++ src/components/Sidebar/SidebarChat.js
@@ -1,7 +1,17 @@
+import { Avatar } from "@material-ui/core";
 import React from "react";
-
+import "./Sidebar.css";
 const SidebarChat = () => {
-  return <div>sidebar chat</div>;
+  return (
+    <div className="sidebarChat">
+      <Avatar className="sidebarChat__avatar" />
+      <div clasName="sidebarChat__info">
+        <h3 className="sidebarChat__name">Jhon</h3>
+        <span className="sidebarChat__message">Last message here</span>
+        <span className="sidebarChat__time">17/11/20 19:38</span>
+      </div>
+    </div>
+  );
 };
 
 export default SidebarChat;
